66232b298234dcd01af61a847698d9108d137285
jpark <jung.hyun.park516@gmail.com>
2021-09-02 16:37:34 -0400

    set up rails create/new controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find_by(id: params[:id])
     end
+
+    def new 
+        @article = Article.new()
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Articcle was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
 end
